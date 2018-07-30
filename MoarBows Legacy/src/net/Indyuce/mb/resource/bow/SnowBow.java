ea34746d45817ea8c79ae18341cda0f20be309f4
Indyuce <29358517+Indyuce@users.noreply.github.com>
2018-07-31 01:43:39 +0200

    Add files via upload

---
+++ MoarBows Legacy/src/net/Indyuce/mb/resource/bow/SnowBow.java
@@ -0,0 +1,53 @@
+package net.Indyuce.mb.resource.bow;
+
+import java.util.Random;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import net.Indyuce.mb.Eff;
+import net.Indyuce.mb.Main;
+import net.Indyuce.mb.api.SpecialBow;
+import net.Indyuce.mb.util.VersionUtils;
+
+public class SnowBow implements SpecialBow {
+	@Override
+	public boolean shoot(EntityShootBowEvent e, Arrow a, Player p, ItemStack i) {
+		e.setCancelled(true);
+		new BukkitRunnable() {
+			double ti = 0;
+
+			public void run() {
+				ti++;
+				if (ti > 20 * e.getForce())
+					cancel();
+
+				Location loc = p.getEyeLocation().clone();
+				Eff.SNOWBALL.display(.2f, .2f, .2f, 0, 6, loc, 100);
+				VersionUtils.sound(p.getLocation(), "ENTITY_SNOWBALL_THROW", 1, 1.5f);
+				Snowball s = p.launchProjectile(Snowball.class);
+				loc.setPitch(loc.getPitch() + new Random().nextInt(3) - 1);
+				loc.setYaw(loc.getYaw() + new Random().nextInt(3) - 1);
+				Vector v = loc.getDirection().multiply(3.3 * e.getForce());
+				s.setVelocity(v);
+			}
+		}.runTaskTimer(Main.plugin, 0, 2);
+		return false;
+	}
+
+	@Override
+	public void hit(EntityDamageByEntityEvent e, Arrow a, Entity p, Player t) {
+	}
+
+	@Override
+	public void land(Arrow a) {
+	}
+}
