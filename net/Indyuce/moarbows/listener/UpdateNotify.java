eb9b7a5ecec8521c5a3d36f4c6e5430275397528
Indyuce <Cym.peyrat@yahoo.fr>
2018-10-19 21:16:06 +0200

    removed a test message

--- net/Indyuce/moarbows/listener/UpdateNotify.java
+++ net/Indyuce/moarbows/listener/UpdateNotify.java
@@ -1,6 +1,5 @@
 package net.Indyuce.moarbows.listener;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,14 +10,11 @@ import net.Indyuce.moarbows.MoarBows;
 
 public class UpdateNotify implements Listener {
 	@EventHandler
-	public void a(PlayerJoinEvent e) {
-		Player p = e.getPlayer();
-		Bukkit.broadcastMessage("test " + p.hasPermission("moarbows.update-notify"));
-		if (!p.hasPermission("moarbows.update-notify"))
-			return;
-
-		if (MoarBows.getSpigotPlugin().isOutOfDate())
-			for (String s : MoarBows.getSpigotPlugin().getOutOfDateMessage())
-				p.sendMessage(ChatColor.GREEN + "(MoarBows) " + s);
+	public void a(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		if (player.hasPermission("moarbows.update-notify"))
+			if (MoarBows.getSpigotPlugin().isOutOfDate())
+				for (String s : MoarBows.getSpigotPlugin().getOutOfDateMessage())
+					player.sendMessage(ChatColor.GREEN + "(SuddenDeath) " + s);
 	}
 }
